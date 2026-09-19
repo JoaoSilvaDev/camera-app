b2e2751dc6a0ab271e1840346814dfebc856f4a7
joao <joaosdev@gmail.com>
2026-09-19 13:51:28 +0100

    update available test

--- main.lua
+++ main.lua
@@ -11,6 +11,8 @@ function love.draw()
   love.graphics.print("Camera app v1", 10, 10)
   if update.available then
     love.graphics.print("UPDATE AVAILABLE", 10, 30)
+  else
+    love.graphics.print("NO UPDATE", 10, 30)
   end
 end
 
